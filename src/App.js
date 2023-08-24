99827953b55278dc607de199bf781f3caf99ce12
unknown <moheb2955@gmail.com>
2023-08-24 09:36:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className="">Hi </h1>
+      <p>1</p>
+      {/* <p>2</p>
+      <p>3</p> */}
+
+    </div>
+  );
+}
+
+export default App;
